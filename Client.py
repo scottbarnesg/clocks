329fe783889a1afca4aea6bbfc92fc220a8c9b89
scottgbarnes <scott.barnes.g@gmail.com>
2020-04-10 18:39:07 -0400

    Client updates

--- Client.py
+++ Client.py
@@ -7,12 +7,12 @@ class Client:
 
 
     def connect(self):
-        TCP_IP = '108.45.131.165'
+        TCP_IP = '192.168.1.218'
         TCP_PORT = 60001
         BUFFER_SIZE = 1024
         MESSAGE = "Connection Request"
         self.s.connect((TCP_IP, TCP_PORT))
-        self.s.send(MESSAGE)
+        self.s.send(MESSAGE.encode())
         data = self.s.recv(BUFFER_SIZE)
         self.playerNum = int(data)
         print("Connected to server")
@@ -21,8 +21,32 @@ class Client:
     def run(self):
         BUFFER_SIZE = 1024
         while True:
-            data = self.s.revc(BUFFER_SIZE)
-            print(data)
+            data = self.s.recv(BUFFER_SIZE)
+
+            if (data.decode() == "Shutdown"):
+                print("Server closed connection... stopping.")
+                self.s.close()
+
+            state = data.decode().split(",")
+            print("Your hand: ")
+            print("--------------------")
+            for value in state:
+                if value[0:2] == "h:":
+                    print(value[2:])
+                if value[0:2] == "c:":
+                    break
+            print("")
+            print("Clock: ")
+            print("--------------------")
+            for value in state:
+                if value[0:2] == "c:":
+                    print(value[2:])
+
 
 
         self.s.close()
+
+if __name__ == "__main__":
+    client = Client()
+    client.connect()
+    client.run()
